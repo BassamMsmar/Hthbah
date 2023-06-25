399566c3f690b028967b77a35151ef97daca0e34
bassammsmar <bassammsmar@gmail.com>
2023-06-25 20:41:53 +0300

    fix color

---
+++ assets/js/popup-img.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+
+    var popupImages = document.querySelectorAll(".popup img");
+    var show = document.querySelector(".show");
+    var imgShow = document.querySelector(".img-show img");
+    var overlay = document.querySelector(".overlay");
+
+    popupImages.forEach(function (popupImage) {
+        popupImage.addEventListener("click", function () {
+            var src = popupImage.getAttribute("src");
+            show.style.display = "block";
+            show.classList.add('active');
+            imgShow.setAttribute("src", src);
+        });
+    });
+
+    overlay.addEventListener("click", function () {
+        show.style.display = "none";
+        show.classList.remove('active');
+    });
+
+});
